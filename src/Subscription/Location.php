589412b1e25147843b9dc59f770a27d9028671da
PINHOf <fpinhocontact@gmail.com>
2022-11-28 21:12:54 +0000

    Adicionado endpoints de CRUD locais

--- src/Subscription/Location.php
+++ src/Subscription/Location.php
@@ -22,8 +22,53 @@ class Location
     }
 
     /**
+     * Creates a new location
+     * https://facturalusa.pt/documentacao/api#subscricao-locais-criar
+     * 
+     * @param   Array   params
+     */
+    public function create($params)
+    {
+        $this->facturalusa->request('subscription/locations/create', 'POST', $params);
+    }
+
+    /**
+     * Updates an existing location
+     * https://facturalusa.pt/documentacao/api#subscricao-locais-actualizar
+     * 
+     * @param   Integer id
+     * @param   Array   params
+     */
+    public function update($id, $params)
+    {
+        $this->facturalusa->request("subscription/locations/{$id}/update", 'POST', $params);
+    }
+
+    /**
+     * Deletes an existing location
+     * https://facturalusa.pt/documentacao/api#subscricao-locais-eliminar
+     * 
+     * @param   Integer id
+     */
+    public function delete($id)
+    {
+        $this->facturalusa->request("subscription/locations/{$id}/delete", 'POST');
+    }
+
+    /**
+     * Finds a location
+     * https://facturalusa.pt/documentacao/api#subscricao-locais-procurar
+     * 
+     * @param   Array   params
+     */
+    public function find($params)
+    {
+        $this->facturalusa->request('subscription/locations/find', 'POST', $params);
+    }
+
+    /**
      * Lists the locations
-     * https://facturalusa.pt/documentacao/api#subscricao
+     * https://facturalusa.pt/documentacao/api#subscricao-locais-lista
      * 
      * @param   Array   params
      */
